9f0786375adaf6245e5b45d9bfaaa6cf6f43641a
BYGranados04 <brennergranados@gmail.com>
2024-02-17 09:32:17 -0600

    -first commint

---
+++ src/main/java/com/mycompany/demoumg/Demoumg.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.demoumg;
+
+/**
+ *
+ * @author Brenner Granados
+ */
+public class Demoumg {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
